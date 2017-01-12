04d277bc900cae1f7883498612940e048d5d6d6b
Gennadiy Khudyakov <lHooFool@gmail.com>
2017-01-13 00:58:50 +0700

    Order items in admin panel

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -13,5 +13,28 @@ ActiveAdmin.register Order do
 #   permitted
 # end
 
+  show do
+    panel "Order" do
+      table_for order do
+        column :price
+        column :address
+        column 'User' do
+          order.user.email unless order.user.nil?
+        end
+        column :created_at
+        column :updated_at
+      end
+    end
+    panel "Order items" do
+      table_for order.order_items do
+        column "name" do |order_item|
+          order_item.item.name
+        end
+        column :quantity
+        column :unit_price
+        column :total_price
+      end
+    end
+  end
 
 end
