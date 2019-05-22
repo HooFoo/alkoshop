a90658f624bb17f1381aa92293e5532b726934a1
Gennadiy Khudyakov <lHooFool@gmail.com>
2019-05-22 13:36:42 +0300

    Fixed item link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def item_link item
-    "/shop/catalog?brand=#{item.brand}&type=#{item.type}&country=#{item.country}#item=#{item.id}"
+    "/shop/catalog?brand=#{item.brand.id}&type=#{item.type.id}&country=#{item.country.id}#item=#{item.id}"
   end
 end
